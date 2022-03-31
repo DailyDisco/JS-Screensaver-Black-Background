3a9096f0ff478efe24c2232c522eedf7e238895b
DailyDisco <97575238+DailyDisco@users.noreply.github.com>
2022-03-30 21:25:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -144,7 +144,7 @@ while (balls.length < 25) {
 
 
 function loop() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
     ctx.fillRect(0, 0, width, height);
  
     for (const ball of balls) { //what does const ball of balls mean?
